0865c02bda2646c7ff337704ec77ea5d7c406e70
robyn-decastro <dylan.decastro@sjsu.edu>
2026-04-03 16:11:16 -0400

    Minor changes to script output

--- run_sprint4.sh
+++ run_sprint4.sh
@@ -15,5 +15,6 @@ touch "${OUTFOLDER}/explicit_ratio_buckets.txt"
 #Step 3, Ratio of artist explicit songs to total songs 
 #The awk begins by creating buckets, then iterates through the data. If a song is explicit, it adds to an explicit dictionary of the artist. It adds the song to another dictionary. Then it compares the ratio and places it in the appropriate bucket. 
 awk -F',' 'BEGIN {zero=0; low=0; med=0; high=0;}
-{if ($9 == "True") explicit[$5]++; songs[$5]++} END {for (a in songs){if (songs[a] > 0) ratio = explicit[a] / songs[a]; else ratio = 0; if (ratio > 0.75) high++; else if (ratio > 0.4) med++; else if (ratio > 0) low++; else zero++;} printf("Artists with: \nZero explicit language:%d\nLittle explicit language:%d\nMedium explicit language:%d\nHigh explicit language:%d", zero, low, med, high)}' "${OUTFOLDER}/temp_sample.csv" > "${OUTFOLDER}/explicit_ratio_buckets.txt"
+{if ($9 == "True") explicit[$5]++; songs[$5]++} END {for (a in songs){if (songs[a] > 0) ratio = explicit[a] / songs[a]; else ratio = 0; if (ratio > 0.75) high++; else if (ratio > 0.4) med++; else if (ratio > 0) low++; else zero++;} printf("Artists with: \nZero explicit language:%d\nLittle explicit language:%d\nMedium explicit language:%d\nHigh explicit language:%d", zero, low, med, high)}' "${OUTFOLDER}/temp_sample.csv" | tee "${OUTFOLDER}/explicit_ratio_buckets.txt"
 
+rm "${OUTFOLDER}/temp_sample.csv"
